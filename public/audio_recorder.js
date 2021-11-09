ac6e1254b20ea8025352f29569e7d2ba9b01bb35
Eliraz Biton <elirazbitonbiton@gmail.com>
2021-11-09 20:28:54 +0200

    init

---
+++ public/audio_recorder.js
@@ -0,0 +1,33 @@
+const recordAudio = () =>
+  new Promise(async resolve => {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+    const mediaRecorder = new MediaRecorder(stream);
+    const audioChunks = [];
+
+    mediaRecorder.addEventListener("dataavailable", event => {
+      audioChunks.push(event.data);
+    });
+
+    const start = () => mediaRecorder.start();
+
+    const stop = () =>
+      new Promise(resolve => {
+        mediaRecorder.addEventListener("stop", () => {
+          const audioBlob = new Blob(audioChunks);
+          const audioUrl = URL.createObjectURL(audioBlob);
+          console.log(audioBlob)
+          const audio = new Audio(audioUrl);
+          audio.loop = true;
+
+          const play = () => audio.play();
+
+          resolve({ audioBlob, audioUrl, play });
+        });
+
+        mediaRecorder.stop();
+      });
+
+    resolve({ start, stop });
+  });
+
+
